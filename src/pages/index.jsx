3dcb90006231352146bae259d41698644ee1bd4a
Eliot Ivan Palacios Hernande <eliotivanhdez@gmail.com>
2020-11-25 18:24:29 -0600

    FEAT: banner and blog section and boot ready

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -31,16 +31,12 @@ import UltramodernEquipment from "../images/se-3.png";
 
 
 //assets
-import banner from "../images/banner-image.jpg";
 import handleft from "../images/icons/hand-btn.png"
 import handRight from "../images/icons/right-btn.png"
 import arrowLeft from "../images/icons/left-arrow-btn.png"
 import arrowRight from "../images/icons/right-arrow-btn.png"
 import facial from "../images/product-image1.png";
 import facial2 from "../images/product-image2.png";
-import circle1 from "../images/se-1.png";
-import circle2 from "../images/se-2.png";
-import circle3 from "../images/se-3.png";
 
 export default function index() {
   const [windowLoaded, setWindowLoaded] = useState(undefined)
@@ -67,21 +63,39 @@ export default function index() {
     <>
       <Layout switchLang={switchLang}>
         <SEO title="Inicio" />
+        <section className="banner">
+            <div className="container">
+              <div></div>
+              <span>
+              <div >
+                <h5>{t("bannerInfoh5")}</h5>
+                <h1>{t("bannerInfoh1")}</h1>
+                <p>{t("bannerInfop1")}</p>
+                <p>{t("bannerInfop2")}</p>
+                <button>{t("bannerInfob1")}</button>
+              </div>
+              </span>
+            </div>
+        </section>
+        <section className="mobile-container">
+          <h5>{t("bannerInfoh5")}</h5>
+          <h1>{t("bannerInfoh1")}</h1>
+          <p>{t("bannerInfop1")}</p>
+          <p>{t("bannerInfop2")}</p>
+          <button>{t("bannerInfob1")}</button>
+        </section>
         <section className="form-info">
           <div className="container">
-            <h3>
-              We Work With High-End Luxury<br/>
-              Brands & Boutiques
-            </h3>
+            <h3>{t("form-info")}</h3>
             <div className="row">
               <div>
                 <div className="form-group">
-                  <input id="name" type="text" className="form-control" placeholder="Name" />
+                  <input id="name" type="text" className="form-control" placeholder={t("name")} />
                 </div>
               </div>
               <div>
                 <div className="form-group">
-                  <input id="email" type="email" className="form-control" placeholder="Email" />
+                  <input id="email" type="email" className="form-control" placeholder={t("email")} />
                 </div>
               </div>
               <div >
@@ -90,43 +104,32 @@ export default function index() {
                     id="phone"
                     type="text"
                     className="form-control"
-                    placeholder="Phone No."
+                    placeholder={t("phone")}
                   />
                 </div>
               </div>
             </div>
-            <div className="form-main-btn">
               <div className="form-btn">
-                <img className="arrow-btn-left" src={arrowLeft} />
+                <img className="arrow-btn-left f" src={arrowLeft} />
                 <div className="free-skin-btn">
-                  <div className="free-skin-btn-anim">
-                    <a href="#;">
+                  <button className="buttonFlaw">
                       <img className="left" src={handleft} />
-                      I Want Flawless
-                      Skin Today
+                    {t("form-info-button")}
                       <img className="right" src={handRight}/>
-                    </a>
-                    <span></span>
-                  </div>
+                    </button>
                 </div>
-                <img className="arrow-btn-right" src={arrowRight} />
+                <img className="arrow-btn-right f" src={arrowRight} />
               </div>
-            </div>
           </div>
         </section>
         <section className="how-we-do">
           <div className="container">
             <div  className="info-row"> 
               <div  className="text-info">
-                <h5>HOW WE DO IT</h5>
-                <h3>Anti-Aging Facial Treatments With The Latest Skincare Technology</h3>
-                <p>Meet the Ultrasonix - a state-of-the-art device that erases wrinkles and defies 
-                  the signs of aging while reducing the size of pores. Ultrasonic pulses 
-                  enhance the performance of skincare products, leaving a rejuvenated and youthful appearance.
-                </p>
-                <p>Cooling and warming vibration technology makes skin smoother than ever,
-                giving your skin an airbrushed appearance.
-                </p>
+                <h5>{t("title-blog-1")}</h5>
+                <h3>{t("sub-title-1")}</h3>
+                <p>{t("p-1-1")}</p>
+                <p>{t("p-2-1")}</p>
               </div>
               <div className="img-info">
                 <img src={facial} alt="anti-agnig facial"/>
@@ -137,62 +140,17 @@ export default function index() {
                 <img src={facial2} alt="anti-agnig facial"/>
               </div>
               <div  className="text-info reverse">
-                <h5>A DEEP DIVE BELOW THE SKIN</h5>
-                <h3>At Venis, we personalize each experience with care, from start to finish</h3>
-                <p>
-                  After providing contact information, one of our skin advisers will be in touch and 
-                  complete a brief evaluation based on your target concerns. We’ll match you with
-                  a top-rated skincare center and customized products that fit your needs, at an 
-                  unbeatable price.
-                </p>
-                <p>
-                We always take care of of our clients we make sure you will always be with a smile!
-                </p>
-                <button>Book a Call</button>
-              </div>
-            </div>
-            <div  className="info-col"> 
-              <div className="info-col-container">
-                <div className="img-info">
-                  <img src={circle1} alt="light therapy"/>
-                </div>
-                <div  className="text-info">
-                  <h3>LED LIGHT THERAPY</h3>
-                  <p>
-                  Cure your skin troubles with different wavelengths of light in 
-                  this advanced treatment.
-                  </p>
-                </div>
-              </div>
-              <div className="info-col-container">
-                <div className="img-info">
-                  <img src={circle2} alt="light therapy"/>
-                </div>
-                <div  className="text-info">
-                  <h3>DIGITAL ANALYSIS DEVICE</h3>
-                  <p>
-                  Get to know your skin like never before with the latest 
-                  technology that's redefining skincare consultations and results globally.
-                  </p>
-                </div>
-              </div>
-              <div className="info-col-container">
-                <div className="img-info">
-                  <img src={circle3} alt="light therapy"/>
-                </div>
-                <div  className="text-info">
-                  <h3>ULTRAMODERN EQUIPMENT AND SKINCARE</h3>
-                  <p>
-                  We use the worlds most advanced systems that maximize the effects of the 
-                  applied serum rewinding the years in your face with a dashing radiant look.s
-                  </p>
-                </div>
+                <h5>{t("title-blog-2")}</h5>
+                <h3>{t("sub-title-2")}</h3>
+                <p>{t("p-1-2")}</p>
+                <p>{t("p-2-2")}</p>
+                <button >{t("button-blog")}</button>
               </div>
             </div>
           </div>
         </section>
 
-                <section className="blog">
+        <section className="blog">
           <div className="container">
             <div>
               <img src={LedLight}  alt="processing"/>
